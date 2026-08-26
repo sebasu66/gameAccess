b065f1f63176cfbe7a4feaf2f5b20a8eeb7314a4
Sebastian Urciuolo <101946505+sebasu66@users.noreply.github.com>
2026-08-26 10:01:14 -0300

    feat: mount custom Tauri chrome and neon polish

--- apps/desktop/src/main.tsx
+++ apps/desktop/src/main.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import WindowChrome from "./WindowChrome";
 import "./styles.css";
 import "./session.css";
 import "./experience.css";
+import "./polish.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <WindowChrome />
     <App />
   </React.StrictMode>,
 );
